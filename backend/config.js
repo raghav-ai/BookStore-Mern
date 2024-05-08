90e9a4ec7c736f3251e3dd272b81c8f6f67c585d
raghav-ai <67466406+raghav-ai@users.noreply.github.com>
2024-05-08 00:30:12 -0400

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const MongoUrl = "mongodb+srv://root1:root@bookstoremern.yscura6.mongodb.net/?retryWrites=true&w=majority";
+export const MongoUrl = "mongodb+srv://****:****@bookstoremern.yscura6.mongodb.net/?retryWrites=true&w=majority";
